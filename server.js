8c88c93b0534b31b4d7e8dd849ddc0e28f78d36d
JonTDean <JonathanTomDean@Gmail.com>
2020-10-01 14:25:13 -0400

    MongoDB setting up

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // Imports & Global Variables
 // // Imports
 const express = require('express');
+const connectDB = require('./config/db');
 // // Global Variables
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Server Logic
+connectDB();
+
+// Route
 app.get('/', (req, res) => res.send('API Running'));
 
+// Listener
 app.listen(port, () => {
 	console.log(`server started on ${port}`);
 });
