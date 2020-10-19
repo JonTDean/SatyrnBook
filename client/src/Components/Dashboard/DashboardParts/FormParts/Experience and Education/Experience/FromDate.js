c27d32d8c0962a6c57ef2b92ca88626477506e68
JonTDean <JonathanTomDean@Gmail.com>
2020-10-19 05:31:56 -0400

    DatePicker getting added

--- client/src/Components/Dashboard/DashboardParts/FormParts/Experience and Education/Experience/FromDate.js
+++ client/src/Components/Dashboard/DashboardParts/FormParts/Experience and Education/Experience/FromDate.js
@@ -3,13 +3,38 @@ import React from 'react';
 // %Styling
 import { formStyle, formInputAlt } from '../../../../../themes/Styles';
 import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+	MuiPickersUtilsProvider,
+	KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 const FromDate = ({ from, onChange }) => {
 	const [selectedDate, setSelectedDate] = React.useState(new Date());
 
 	return (
 		<div className={formStyle().formDiv}>
-			<TextField
+			<MuiPickersUtilsProvider utils={DateFnsUtils}>
+				<KeyboardDatePicker
+					disableToolbar
+					inputprops={formInputAlt}
+					inputlabelprops={formInputAlt}
+					variant="inline"
+					format="MM/dd/yyyy"
+					margin="normal"
+					id="from-field"
+					label="From"
+					helperText="When did you begin work at this company?"
+					className={formStyle().textField}
+					name="from"
+					value={from}
+					onChange={onChange}
+					// KeyboardButtonProps={{
+					// 	'aria-label': 'change date',
+					// }}
+				/>
+			</MuiPickersUtilsProvider>
+			{/* <TextField
 				inputprops={formInputAlt}
 				inputlabelprops={formInputAlt}
 				id="from-field"
@@ -19,7 +44,7 @@ const FromDate = ({ from, onChange }) => {
 				name="from"
 				value={from}
 				onChange={onChange}
-			/>
+			/> */}
 		</div>
 	);
 };
